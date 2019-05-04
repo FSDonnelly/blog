f7026f7aa9c3cacded966d81bc148ce9b63b3bfd
FSDonnelly <donnelly_frank@yahoo.com>
2019-05-03 22:05:42 -0700

    setup UserHeader component

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -9,10 +9,20 @@ class UserHeader extends Component {
     }
 
     render() {
+        const user = this.props.users.find(user => user.id === this.props.userId);
+
+        if (!user) {
+            return null;
+        }
+
         return (
-            <div>User Header</div>
+            <div className="header">{user.name}</div>
         );
     }
 };
 
-export default connect(null, { fetchUser })(UserHeader);
+const mapStateToProps = state => {
+    return { users: state.users };
+};
+
+export default connect(mapStateToProps, { fetchUser })(UserHeader);
